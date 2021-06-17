08fa03ff372fcb7e0e3ac30742d1001c9670f583
Fredrik Moberg <fredrik.moberg@knowit.se>
2021-06-17 17:46:01 +0200

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '96d681eb4f0d67d2fa388c32388507ef'),
+    },
+  },
+});
